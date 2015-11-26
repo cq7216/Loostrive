757816b75d2f9e43c81fd8e47050f261a2b488de
cq7216 <1457063@qq.com>
2015-11-27 07:50:12 +0800

    添加404页面声音

--- 404.php
+++ 404.php
@@ -8,9 +8,10 @@
     <div class="mainleft">
 		<div class="article_container row  box">
            <div class="third centered" style="text-align:center; margin:50px auto;">
-				<h2><center>抱歉，您打开的页面未能找到。</center></h2>
+				<h2><center>抱歉，找不到这个页面。</center></h2>
         		<div class="context">
        			  <center><a href="<?php bloginfo('siteurl');?>" title="返回首页"><img src="<?php bloginfo('template_directory'); ?>/images/404.gif" alt="Error 404 - Not Found" /></a></center>
+              <audio src="/wp-content/themes/Loostrive/404.mp3" autoplay="autoplay"> </audio>
             	</div>
 			</div>
         </div>
