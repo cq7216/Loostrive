4880663a93489a8760f6e924051ad199d8f2c4ce
Taoxiaozhong <1457063@qq.com>
2017-03-31 08:23:17 +0800

    减少底部文章显示数目

--- footer.php
+++ footer.php
@@ -5,7 +5,52 @@
 			<div class="col-xs-4">
 				<div id="tag_cloud-2" class="widget widget_tag_cloud">
 					<h3 class="widget-title"><span>热门标签</span></h3>
-					<?php wp_tag_cloud('smallest=12&largest=12'); ?>
+					<!-- <?php wp_tag_cloud('smallest=12&largest=12'); ?> -->
+          <a href="http://taoxiaozhong.com/tag/app" class="tag-link-159" title="6个话题" style="font-size: 12pt;">app</a>
+          <a href="http://taoxiaozhong.com/tag/ppt" class="tag-link-57" title="3个话题" style="font-size: 12pt;">ppt</a>
+          <a href="http://taoxiaozhong.com/tag/ss" class="tag-link-43" title="4个话题" style="font-size: 12pt;">ss</a>
+          <a href="http://taoxiaozhong.com/tag/web%e5%89%8d%e7%ab%af" class="tag-link-169" title="11个话题" style="font-size: 12pt;">web前端</a>
+          <a href="http://taoxiaozhong.com/tag/%e4%b8%8b%e8%bd%bd" class="tag-link-167" title="5个话题" style="font-size: 12pt;">下载</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%85%8d%e8%b4%b9" class="tag-link-46" title="4个话题" style="font-size: 12pt;">免费</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%88%9b%e6%84%8f" class="tag-link-162" title="6个话题" style="font-size: 12pt;">创意</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%8a%a8%e6%bc%ab" class="tag-link-87" title="4个话题" style="font-size: 12pt;">动漫</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%8e%9f%e5%88%9b" class="tag-link-91" title="4个话题" style="font-size: 12pt;">原创</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%9b%bd%e5%a4%96%e7%bd%91%e7%ab%99" class="tag-link-36" title="18个话题" style="font-size: 12pt;">国外网站</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%9b%be%e7%89%87" class="tag-link-174" title="6个话题" style="font-size: 12pt;">图片</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%9c%a8%e7%ba%bf%e5%90%ac%e6%ad%8c" class="tag-link-15" title="11个话题" style="font-size: 12pt;">在线听歌</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%9c%a8%e7%ba%bf%e5%ba%94%e7%94%a8" class="tag-link-209" title="6个话题" style="font-size: 12pt;">在线应用</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%9c%a8%e7%ba%bf%e6%9c%8d%e5%8a%a1" class="tag-link-49" title="7个话题" style="font-size: 12pt;">在线服务</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%ad%a6%e4%b9%a0" class="tag-link-164" title="6个话题" style="font-size: 12pt;">学习</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%b7%a5%e5%85%b7" class="tag-link-137" title="23个话题" style="font-size: 12pt;">工具</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%ba%94%e7%94%a8" class="tag-link-157" title="29个话题" style="font-size: 12pt;">应用</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%bc%80%e6%ba%90" class="tag-link-48" title="3个话题" style="font-size: 12pt;">开源</a>
+          <a href="http://taoxiaozhong.com/tag/%e5%bd%b1%e8%a7%86" class="tag-link-141" title="13个话题" style="font-size: 12pt;">影视</a>
+          <a href="http://taoxiaozhong.com/tag/%e6%89%8b%e6%9c%ba" class="tag-link-158" title="7个话题" style="font-size: 12pt;">手机</a>
+          <a href="http://taoxiaozhong.com/tag/%e6%91%84%e5%bd%b1" class="tag-link-13" title="7个话题" style="font-size: 12pt;">摄影</a>
+          <a href="http://taoxiaozhong.com/tag/%e6%92%ad%e6%94%be%e5%99%a8" class="tag-link-68" title="5个话题" style="font-size: 12pt;">播放器</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%94%b5%e5%8f%b0" class="tag-link-18" title="5个话题" style="font-size: 12pt;">电台</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%94%b5%e5%bd%b1" class="tag-link-95" title="6个话题" style="font-size: 12pt;">电影</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%a4%be%e5%8c%ba" class="tag-link-85" title="9个话题" style="font-size: 12pt;">社区</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%a7%91%e5%ad%a6%e4%b8%8a%e7%bd%91" class="tag-link-134" title="4个话题" style="font-size: 12pt;">科学上网</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%a8%8b%e5%ba%8f%e5%91%98" class="tag-link-153" title="5个话题" style="font-size: 12pt;">程序员</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%ab%99%e9%95%bf" class="tag-link-50" title="14个话题" style="font-size: 12pt;">站长</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%ab%99%e9%95%bf%e6%8e%a8%e8%8d%90" class="tag-link-175" title="10个话题" style="font-size: 12pt;">站长推荐</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%b4%a0%e6%9d%90" class="tag-link-166" title="7个话题" style="font-size: 12pt;">素材</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%bc%96%e7%a8%8b" class="tag-link-152" title="6个话题" style="font-size: 12pt;">编程</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%bd%91%e7%bb%9c%e5%ae%89%e5%85%a8" class="tag-link-37" title="4个话题" style="font-size: 12pt;">网络安全</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%bd%91%e7%bb%9c%e6%94%bb%e5%87%bb" class="tag-link-35" title="3个话题" style="font-size: 12pt;">网络攻击</a>
+          <a href="http://taoxiaozhong.com/tag/%e7%bd%91%e9%a1%b5%e8%ae%be%e8%ae%a1" class="tag-link-33" title="4个话题" style="font-size: 12pt;">网页设计</a>
+          <a href="http://taoxiaozhong.com/tag/%e8%87%aa%e5%aa%92%e4%bd%93" class="tag-link-83" title="4个话题" style="font-size: 12pt;">自媒体</a>
+          <a href="http://taoxiaozhong.com/tag/%e8%a7%86%e9%a2%91" class="tag-link-89" title="9个话题" style="font-size: 12pt;">视频</a>
+          <a href="http://taoxiaozhong.com/tag/%e8%a7%86%e9%a2%91%e6%95%99%e7%a8%8b" class="tag-link-26" title="3个话题" style="font-size: 12pt;">视频教程</a>
+          <a href="http://taoxiaozhong.com/tag/%e8%ae%be%e8%ae%a1" class="tag-link-165" title="6个话题" style="font-size: 12pt;">设计</a>
+          <a href="http://taoxiaozhong.com/tag/%e8%b5%84%e6%ba%90" class="tag-link-183" title="4个话题" style="font-size: 12pt;">资源</a>
+          <a href="http://taoxiaozhong.com/tag/%e8%b5%84%e8%ae%af" class="tag-link-163" title="9个话题" style="font-size: 12pt;">资讯</a>
+          <a href="http://taoxiaozhong.com/tag/%e8%b6%a3%e7%ab%99" class="tag-link-172" title="8个话题" style="font-size: 12pt;">趣站</a>
+          <a href="http://taoxiaozhong.com/tag/%e8%bd%af%e4%bb%b6" class="tag-link-136" title="7个话题" style="font-size: 12pt;">软件</a>
+          <a href="http://taoxiaozhong.com/tag/%e9%98%85%e8%af%bb" class="tag-link-201" title="6个话题" style="font-size: 12pt;">阅读</a>
+          <a href="http://taoxiaozhong.com/tag/%e9%9f%b3%e4%b9%90" class="tag-link-121" title="16个话题" style="font-size: 12pt;">音乐</a>
+          <a href="http://taoxiaozhong.com/tag/%e9%bb%91%e5%ae%a2" class="tag-link-34" title="5个话题" style="font-size: 12pt;">黑客</a>
 				</div>
 			</div><!-- /footer-widgeted-1 -->
 			<div class="col-xs-4">
@@ -20,7 +65,7 @@
 							add_filter('posts_where', 'mostweek'); 
 						?>
 						<?php
-							query_posts("v_sortby=views&caller_get_posts=1&orderby=date&v_orderby=desc&showposts=10") 
+							query_posts("v_sortby=views&caller_get_posts=1&orderby=date&v_orderby=desc&showposts=7") 
 						?>
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 						<li><a href="<?php the_permalink() ?>" title="<?php the_title() ?>"><?php the_title() ?></a></li>
@@ -35,17 +80,17 @@
 						<a href="http://www.iztwp.com/" title="小众网站">淘小众</a>，专注于分享国内外优秀网站，小众是针对大众而言，顾名思义，就是在一个小范围内流行的网站。有的网站不被大多数人知道，甚至主流导航网站也没有收录它们，但它们在一个小范围内却是非常有名，无论用户体验还是内容都堪称精品。
 						<p style="color: #F0EB84;text-align:left;">温馨提示：本站所有搜集网站均来自互联网，对网站的介绍属作者个人观点，仅供参考。若发现网站链接错误，烦请通报管理员。</p>
 					</div>
-				</div>
+				</div><!-- 
 				<div id="text-5" class="widget widget_text">
 					<h3 class="widget-title"><span>联系我们</span></h3>
 					<div class="textwidget">QQ：1457063 <br>E-Mail：1457063#qq.com（用@替换#）</div>
-				</div>
+				</div> -->
 				<div id="text-6" class="widget widget_text">
 					<h3 class="widget-title"><span>支持淘小众</span></h3>
 					<a href="http://shang.qq.com/wpa/qunwpa?idkey=4b41fbbb002717c3f51cc84f38912b3aefd73ea1c4e62f2136c761d57b7f2680" target="_blank"><img alt="img" src="../wp-content/themes/Loostrive/images/ico_group.png"></a>
 					<a href="http://www.aliyun.com/product/ecs/?spm=5176.383338.201.11.EmuIDM" target="_blank"><img alt="img" src="../wp-content/themes/Loostrive/images/ico_aliyun.png" _hover-ignore="1"></a>
 					<a href="https://portal.qiniu.com/signup?code=3lmd5owdjqr0y" target="_blank"><img alt="img" src="../wp-content/themes/Loostrive/images/ico_qiniu.png"></a>
-					<input name="pay" type="image" value="转账" src="../wp-content/themes/Loostrive/images/zhifubao.png">
+					<!-- <input name="pay" type="image" value="转账" src="../wp-content/themes/Loostrive/images/zhifubao.png"> -->
 			</div>
 		</div><!-- /footer-widgeted-3 -->
 	</div>
@@ -58,11 +103,11 @@
 	<?php } ?>
 	<?php } else {} ?>
 	<div class="copyright">
-		<p>CQ工作室荣誉出品  |  版权所有 <?php echo comicpress_copyright();?> <a href="<?php bloginfo('url');?>/"><strong>
+		<p><?php echo comicpress_copyright();?>
 			<?php bloginfo('name');?>
-		</strong></a>  |  <?php if (get_option('strive_beian') == 'Display') { ?>
+		    • 联系：1457063@qq.com  • 备案信息：<?php if (get_option('strive_beian') == 'Display') { ?>
 		<a href="http://www.miitbeian.gov.cn/" rel="external"><?php echo stripslashes(get_option('strive_beianhao')); ?></a>
-		<?php { echo '  |  '; } ?>
+		<?php { echo '   •  '; } ?>
 		<?php } else {} ?>
 		<?php if (get_option('strive_tj') == 'Display') { ?>
 		<?php echo stripslashes(get_option('strive_tjcode')); ?>
@@ -82,8 +127,9 @@
 	<div id="footer_in" class="container">
 		<p class="f_sml">
 			<!-- 纯代码实现网站运行多少天 -->
-          <script type="text/javascript">// <![CDATA[
-          var bkyx= new Date("12/27/2013");
+          <script type="text/javascript">
+          // <![CDATA[
+          var bkyx= new Date("11/11/2015");
           	var now = new Date();
           	var xzbk = now.getTime() - bkyx.getTime();
           	var bksj = Math.floor(xzbk / (1000 * 60 * 60 * 24));
